e043553b67af7c025acedbdb4570400fd6caf2b3
GuilhermeDaBolsa <Guilherme_Giordani@live.com>
2023-10-26 00:05:22 -0300

    formating concorde data

--- Program.cs
+++ Program.cs
@@ -34,9 +34,14 @@ var MAX_CITY_Y_COORD_PROPORTION = 3;
 
 
 
+
+
+//-- RUN BENCHMARKS ON TEST FILES (AND SAVE RESULTS) --//
+//COMMENT IF RESULTS WERE ALREADY GENERATED
+
 var sampleDirectories = Directory.GetDirectories(TESTS_BASE_FOLDER).ToList();
 
-sampleDirectories = sampleDirectories.Where(d => d.EndsWith("_15")).ToList();
+sampleDirectories = sampleDirectories.Where(d => d.EndsWith("_20")).ToList();
 
 //SORTING SAMPLES DIRECTORIES IN ORDER OF INSTANCE SIZES
 //sampleDirectories.Sort((x, y) => Int32.Parse(x.Substring(x.LastIndexOf("_")+1)) - Int32.Parse(y.Substring(y.LastIndexOf("_")+1)));
@@ -61,3 +66,69 @@ foreach (var directory in sampleDirectories) {
             => benchmarker.RunBenchmark(TSP, $"{directory}/{benchmarker.name}/{TSP.name}.ans"));
     }
 }
+
+
+
+
+
+
+//-- FORMAT CONCORDE DATA --//
+
+/*var sampleDirectories = Directory.GetDirectories(TESTS_BASE_FOLDER).Where(d => Int32.Parse(d.Substring(d.LastIndexOf("_") + 1)) > 30).ToList();
+sampleDirectories.Sort((x, y) => Int32.Parse(x.Substring(x.LastIndexOf("_") + 1)) - Int32.Parse(y.Substring(y.LastIndexOf("_") + 1)));
+
+var concordeDirectories = 
+    sampleDirectories
+        .Select(d => Directory.Exists($"{d}/Concorde") ? $"{d}/Concorde" : "REMOVE")
+        .Where(cd => cd != "REMOVE")
+        .ToList();
+
+
+concordeDirectories.ForEach(cd => {
+
+    var sampleFiles = Directory.GetFiles(cd, "*.ans");
+
+    foreach (var sampleFile in sampleFiles) {
+
+        if (!File.Exists(sampleFile))
+            return;
+
+        var fileNameAndExtension = sampleFile.Substring(sampleFile.LastIndexOf("Random"));
+
+        var fileName = fileNameAndExtension.Substring(0, fileNameAndExtension.Length - 4);
+
+        IEnumerable<string> lines = File.ReadLines(sampleFile);
+        var linesIterator = lines.GetEnumerator();
+        linesIterator.MoveNext();
+
+        float timeInSec = float.Parse(linesIterator.Current);
+        long timeInMs = (long)(timeInSec * 1000);
+
+        linesIterator.MoveNext();
+
+        int size = int.Parse(linesIterator.Current);
+        var indexes = new List<int>(size);
+
+        while (linesIterator.MoveNext()) {
+            if(!string.IsNullOrWhiteSpace(linesIterator.Current))
+                indexes.Add(int.Parse(linesIterator.Current));
+        }
+
+        indexes.Add(0);
+
+        //WRITE FILE
+        using (StreamWriter writer = File.CreateText(sampleFile)) {
+            writer.WriteLine($"NAME: {fileName}");
+            writer.WriteLine($"ELLAPSED_TIME_(ms): {timeInMs}");
+            writer.WriteLine($"TOUR_SIZE: {size}");
+            writer.WriteLine("TOUR_SECTION");
+
+            for (int i = 0; i < indexes.Count; i++) {
+                writer.WriteLine($"{indexes[i]}");
+            }
+
+            writer.WriteLine("EOF");
+        }
+    }
+});
+*/
